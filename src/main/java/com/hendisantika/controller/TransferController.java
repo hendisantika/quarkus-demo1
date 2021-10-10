5bc06599f6debc16af7cbd2d43bc586e3db819da
Hendi Santika <hendisantika@gmail.com>
2021-10-10 18:43:22 +0700

    #12 - Add create in TransferController

--- src/main/java/com/hendisantika/controller/TransferController.java
+++ src/main/java/com/hendisantika/controller/TransferController.java
@@ -1,13 +1,16 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.dto.TransferDto;
 import com.hendisantika.service.TransferService;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,4 +35,27 @@ public class TransferController {
                 .ok(transferService.findAll())
                 .build();
     }
+
+    @POST
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(TransferDto transferDto) {
+        try {
+            Long id = transferService.create(transferDto);
+            return Response // 200
+                    .ok(
+                            new HashMap() {{
+                                put("id", id);
+                            }})
+                    .build();
+        } catch (Exception ex) {
+            return Response // error 400
+                    .status(400).entity(
+                            new HashMap() {{
+                                put("error", "failed to insert");
+                            }})
+                    .build();
+        }
+    }
+
 }
